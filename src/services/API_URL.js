5edee8e3859afbf2623408f005fbe063b54cdcda
Jennifer Tan <housingmatchnyc@gmail.com>
2024-01-03 12:24:54 -0500

    updated URL

--- src/services/API_URL.js
+++ src/services/API_URL.js
@@ -1,3 +1,5 @@
 // export const API_URL = "http://localhost:3000"
 
-export const API_URL = "https://project3-hm.adaptable.app"
+// export const API_URL = "https://project3-hm.adaptable.app"
+
+export const API_URL = "https://housingmatch.netlify.app/"
